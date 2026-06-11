74fd0bc5bd9cf8f589dd7c2a718c10237e88e163
Behzad Shabanifard <b3hzadsh@gmail.com>
2026-06-11 14:15:30 +0330

    test: use mocktail to mock Dio in RssDataSource tests

--- test/data/sources/rss_data_source_test.dart
+++ test/data/sources/rss_data_source_test.dart
@@ -1,9 +1,21 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:news_aggregator/data/sources/rss_data_source.dart';
 import 'package:dio/dio.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockDio extends Mock implements Dio {}
+class MockResponse extends Mock implements Response {}
 
 void main() {
-  test('should parse RSS XML correctly', () {
+  late MockDio mockDio;
+  late RssDataSource dataSource;
+
+  setUp(() {
+    mockDio = MockDio();
+    dataSource = RssDataSource(dio: mockDio);
+  });
+
+  test('should parse RSS XML correctly with all fields', () async {
     const xml = '''
     <rss version="2.0">
       <channel>
@@ -15,8 +27,20 @@ void main() {
         </item>
       </channel>
     </rss>''';
-    final dataSource = RssDataSource(dio: Dio());
-    final items = dataSource.parseRss(xml, "Test Source");
+
+    when(() => mockDio.get(any())).thenAnswer((_) async => Response(
+      data: xml,
+      statusCode: 200,
+      requestOptions: RequestOptions(path: ''),
+    ));
+
+    final items = await dataSource.fetchFeed("https://test.com/rss", "Test Source");
+    
+    expect(items.length, 1);
     expect(items.first.title, "Test News");
+    expect(items.first.content, "Test Summary");
+    expect(items.first.remoteId, "https://test.com/1");
+    expect(items.first.sourceName, "Test Source");
+    // publishDate check will be added in Task 4
   });
 }
