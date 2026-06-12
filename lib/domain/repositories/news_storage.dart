780540ce1ff08b487dabcfc72109e77500644cab
Behzad Shabanifard <b3hzadsh@gmail.com>
2026-06-12 15:12:36 +0330

    feat: add category methods to NewsStorage interface

--- lib/domain/repositories/news_storage.dart
+++ lib/domain/repositories/news_storage.dart
@@ -1,9 +1,13 @@
-import '../../domain/entities/news_item.dart';
+import '../entities/news_item.dart';
+import '../entities/category.dart';
 
 abstract class NewsStorage {
   Future<void> insertMany(List<NewsItem> items);
   List<NewsItem> getAll();
   Set<String> getAllRemoteIds();
   Stream<List<NewsItem>> watchAllItems();
+  Future<List<Category>> getAllCategories();
+  Future<void> seedCategories(List<Category> categories);
+  Stream<List<NewsItem>> watchItemsByCategory(int categoryId);
   Future<void> close();
 }
